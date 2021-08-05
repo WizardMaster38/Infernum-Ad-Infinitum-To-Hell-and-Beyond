1afdda947453f9e3d6caca6903c5e0c1174345cd
AC69 <63432187+WizardMaster38@users.noreply.github.com>
2021-08-05 02:35:54 +0000

    redoing text

--- Main.java
+++ Main.java
@@ -140,8 +140,8 @@ class Main {
 
         System.out.println(mainMenu);
         String nextInput = input.nextLine();
-        if ((nextInput.toLowerCase() == "start game") || (nextInput.toLowerCase() == "load game") || (nextInput.toLowerCase() == "settings") || (nextInput.toLowerCase() == "exit")) {
-            System.out.println(nextInput + " is an invalid input.");
+        if (nextInput.toLowerCase() == "start game") {
+            System.out.println("Game starting..");
         }
     }
 }
